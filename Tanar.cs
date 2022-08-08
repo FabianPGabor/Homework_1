02be328a95fea613165e341c995aa93a1d669202
kaktu <kaktu@LAPTOP-66V35BJJ>
2022-08-08 14:14:41 +0200

    Add project files.

---
+++ Tanar.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace Karesz
+{
+    public partial class Form1 : Form
+    {
+        string betöltendő_pálya = "palya001.txt";
+
+        void TANÁR_ROBOTJAI()
+        {
+            new Robot("Karesz", 10, 10, 10, 10, 0, 1, 1, 2);
+
+
+            Robot janesz = new Robot("Janesz", Robot.képkészlet_lilesz, 0, 0, 0, 0, 15, 3, 29, 0);
+
+            janesz.Feladat = delegate ()
+            {
+                int a = 0;
+                a = janesz.UltrahangSzenzor();
+                for (int i = 0; i < janesz.Köveinek_száma_ebből(hó); i++)
+                {
+                    janesz.Lőjj();
+                    for (int j = 0; j < 3; j++)
+                    {
+                        janesz.Várj();
+                        a = janesz.UltrahangSzenzor();
+                    }
+                    
+                }
+            };
+
+            /*Robot anti = new Robot("Anti", new int[] { 0, 0, 0, 0, 10 }, 14, 12, 0);
+
+            anti.Feladat = delegate ()
+            {
+				for (int i = 0; i < 4; i++)
+				{
+                    anti.Lőjj();
+                    anti.Fordulj(jobbra);
+				}
+            };*/
+        }
+    }
+}
